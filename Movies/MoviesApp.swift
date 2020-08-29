5f3d10fbae7621d6aedf7a53f827141bc16457e7
Anderson <samadhihuang@gmail.com>
2020-08-29 11:02:18 +0800

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Anderson  on 2020/8/29.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
